eb680d7afd12d5a9a53c99ffc86711f3d55db776
Perchusha <perch.anton@gmail.com>
2021-04-22 19:45:58 +0300

    [dev] In progress

--- plugins/samplesLinksNormalize.js
+++ plugins/samplesLinksNormalize.js
@@ -45,8 +45,8 @@ const getFiles = (dir, files_) => {
 
 const updateFileData = file => {
 	const fileData = fs.readFileSync(file.filePath, 'utf-8');
-	let newData = fileData.replace(/^@relatedsample: (https:\/\/\w+.\w+.\w+\/\w+)\s(.*)$/gm, i => {
-		const signature = /^@relatedsample: (https:\/\/\w+.\w+.\w+\/\w+)\s(.*)$/gm.exec(fileData);
+	let newData = fileData.replace(/^@relatedsample:\s+(https:\/\/\w+.\w+.\w+\/\w+)\s(.*)$/gm, i => {
+		const signature = /^@relatedsample:\s+(https:\/\/\w+.\w+.\w+\/\w+)\s(.*)$/gm.exec(fileData);
 		return signature ? `**Related sample**: [${signature[2]}](${signature[1]})` : i;
 	});
 	newData = newData.replace(/^{{\w+\s+(https:\/\/\w+.\w+.\w+\/\w+)\s(.*)}}$/gm, i => {
