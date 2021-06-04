372d4be391b833b363328618bc8f573f2c7b1548
Tatyana <->
2021-06-04 11:38:47 +0300

    [update] PRO version only block

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -55,7 +55,7 @@ const onBraceNotationMatch = (data, { key, fullMatch }) => {
         case 'note':
             return `:::note\n${data}\n:::`;
         case 'pronote':
-            return `:::danger Pro Note\n${data}\n:::`;
+            return `:::tip Pro version only\n${data}\n:::`;
         case 'editor':
             return data;
         default:
