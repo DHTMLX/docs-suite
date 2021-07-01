529b6eec9fd7d4ddfe811dd381249a746fcdb57a
Perchusha <perch.anton@gmail.com>
2021-07-01 17:22:07 +0300

    [fix] Refactoring code

--- src/theme/DocPage/index.jsx
+++ src/theme/DocPage/index.jsx
@@ -1,16 +1,25 @@
-import React, { useEffect } from 'react';
-import { MDXProvider } from '@mdx-js/react';
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import React, {useEffect} from 'react';
+import {MDXProvider} from '@mdx-js/react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import renderRoutes from '@docusaurus/renderRoutes';
 import Layout from '@theme/Layout';
 import DocSidebar from '@theme/DocSidebar';
 import MDXComponents from '@theme/MDXComponents';
 import NotFound from '@theme/NotFound';
-import { matchPath } from '@docusaurus/router';
+import {matchPath} from '@docusaurus/router';
 import Head from '@docusaurus/Head';
-import styles from './styles.module.scss';
+import styles from './styles.module.css';
 
-function DocSearchVersionHeader({ version, isLast }) {
+// This theme is not coupled to Algolia, but can we do something else?
+// Note the last version is also indexed with "last", to avoid breaking search on new releases
+// See https://github.com/facebook/docusaurus/issues/3391
+function DocSearchVersionHeader({version, isLast}) {
 	const versions = isLast ? [version, 'latest'] : [version];
 	return (
 		<Head>
@@ -36,27 +45,23 @@ function resetDisqusPlugin(path) {
 			reload: true,
 			config: function () {
 				this.page.identifier = path;
-				this.page.url = "https://docs.dhtmlx.com" + path;
+				this.page.url = "https://docs.dhtmlx.com"+path;
 				this.page.title = "";
 			}
 		});
 	}
 };
 
-function DocPageContent({ currentDocRoute, versionMetadata, children }) {
-	const { siteConfig, isClient } = useDocusaurusContext();
-	const { permalinkToSidebar, docsSidebars, version, isLast } = versionMetadata;
+function DocPageContent({currentDocRoute, versionMetadata, children}) {
+	const {siteConfig, isClient} = useDocusaurusContext();
+	const {permalinkToSidebar, docsSidebars, version, isLast} = versionMetadata;
 	const sidebarName = permalinkToSidebar[currentDocRoute.path];
 	const sidebar = docsSidebars[sidebarName];
 
 	useEffect(() => {
-		if (!window.disqus_enabled) {
+		if (!window.disqus_enabled){
 			var disqus_shortname = 'dhxdocumentation';
-			var dsq = document.createElement('script');
-			dsq.type = 'text/javascript';
-			dsq.async = true;
-			dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-			(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+			var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
 
 			window.disqus_enabled = true;
 		}
@@ -66,13 +71,15 @@ function DocPageContent({ currentDocRoute, versionMetadata, children }) {
 
 	return (
 		<>
-			<DocSearchVersionHeader version={version} isLast={isLast}/>
+			<DocSearchVersionHeader version={version} isLast={isLast} />
 			<Layout key={isClient}>
 				<div className={styles.docPage}>
 					{sidebar && (
 						<div className={styles.docSidebarContainer} role="complementary">
 							<DocSidebar
 								key={
+									// Reset sidebar state on sidebar changes
+									// See https://github.com/facebook/docusaurus/issues/3414
 									sidebarName
 								}
 								sidebar={sidebar}
@@ -93,9 +100,9 @@ function DocPageContent({ currentDocRoute, versionMetadata, children }) {
 	);
 }
 
-const DocPage = props => {
+function DocPage(props) {
 	const {
-		route: { routes: docRoutes },
+		route: {routes: docRoutes},
 		versionMetadata,
 		location,
 	} = props;
