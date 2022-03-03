327c1c8aa03d46517d6c34d845777cdc4d0a7355
Serhii Pylypchuk <lingvopoligraf@yandex.by>
2022-03-03 11:33:46 +0300

    [update] hide the 'Edit this page' link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -286,7 +286,7 @@ module.exports = {
                 docs: {
                     sidebarPath: require.resolve('./sidebars.js'),
                     // Please change this to your repo.
-                    editUrl: 'https://github.com/DHTMLX/docs-suite/edit/master/',
+                    //editUrl: 'https://github.com/DHTMLX/docs-suite/edit/master/',
                     routeBasePath: '/',
                 },
                 // blog: {
